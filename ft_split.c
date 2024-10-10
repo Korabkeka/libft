23216401cb29c107626ca34cf40ae18b2c1d27ae
kkeka <kkeka@student.42lausanne.ch>
2024-10-10 22:06:51 +0200

    split

---
+++ ft_split.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkeka <kkeka@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 20:14:20 by kkeka             #+#    #+#             */
+/*   Updated: 2024/10/10 22:06:06 by kkeka            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+static void countwords(char const *s, char c, int *counter)
+{
+    int i;
+
+    i = 0;
+    while(s[i])
+    {
+        if(s[i++] == (const char)c)
+            (*counter)++;
+    }
+    (*counter)++;
+}
+
+static char *saveword(char const *s, char c, int *current)
+{
+    int len;
+    int start;
+    int i;
+    char    *str;
+
+    len = 0;
+    i = 0;
+    start = *current;
+    while (s[*current] && s[*current] != c)
+    {
+        len++;
+        (*current)++;
+    }
+    (*current)++;
+    str = (char *)malloc((len + 1) * sizeof(char));
+    if (!str)
+        return (NULL);
+    while (start < (*current) - 1)
+        str[i++] = s[start++];
+    str[i] = 0;
+    return (str);
+}
+
+char **ft_split(char const *s, char c)
+{
+    int     i;
+    int     wordcounter;
+    int     current_split_start;
+    char    **strarr;
+
+    i = 0;
+    wordcounter = 0;
+    current_split_start = 0;
+    countwords(s, c, &wordcounter);
+    strarr = (char **)malloc(wordcounter * sizeof(char *) + 1);
+    if(!strarr)
+        return (NULL);
+    while (i < wordcounter)
+        strarr[i++] = saveword(s, c, &current_split_start);
+    strarr[i] = 0;
+    return (strarr);
+}
+
+/*int main(void)
+{
+    char    **arr = ft_split("bonjour comment ca va ?", ' ');
+    int i = 0;
+    while(arr[i])
+        printf("%s\n", arr[i++]);
+}*/
