14a2f81e77979163ec9597ca8f7915e593921b5b
kkeka <kkeka@student.42lausanne.ch>
2024-10-22 15:56:36 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -12,14 +12,78 @@
 
 #include "libft.h"
 
+static int	countwords(char const *s, char c)
+{
+	int	i;
+	int count;
+	int currentlyinword;
+
+	i = 0;
+	count = 0;
+	currentlyinword = 0;
+	while (s[i])
+	{
+		if(s[i] != c && !currentlyinword)
+		{
+			count++;
+			currentlyinword = 1;
+		}
+		if(s[i] == c)
+			currentlyinword = 0;
+		i++;
+	}
+	return (count);
+}
+
+static void	init_vars(int *i, int *j, int *currentlyinword)
+{
+	*i = 0;
+	*j = 0;
+	*currentlyinword = 0;
+}
+
+static char	*addword(const char *s, char c, int start_index)
+{
+
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**res;
+	int		currentlyinword;
+	int		i;
+	int		j;
+
+	res = malloc((countwords(s, c) + 1) * sizeof(char *));
+	init_vars(&i, &j, &currentlyinword);
+	if (!res)
+		return (NULL);
+	while (s[i])
+	{
+		if(s[i] != c && !currentlyinword)
+		{
+			res[j] = addword(s, c, i);
+			if(!res[j])
+				return(free_all(res, j));
+			currentlyinword = 1;
+			j++;
+		}
+		if(s[i] == c)
+			currentlyinword = 0;
+		i++;
+	}
+	res[j] = NULL;
+	return (res);
 }
-/*int main(void)
+
+#include <stdio.h>
+
+int main(void)
 {
-	char    **arr = ft_split("bonjour comment ca va ?", ' ');
+	/*char    **arr = ft_split("bonjour comment ca va ?", ' ');
 	int i = 0;
 	while(arr[i])
-		printf("%s\n", arr[i++]);
-}*/
+		printf("%s\n", arr[i++]);*/
+	/*char s[] = "    ";
+	printf("%i\n", countwords(s, ' '));*/
+}
